3e7fc59e0064287fc0534fdb82da99d92943fd63
Huy Phạm <quanghuybk25@gmail.com>
2025-12-07 20:06:53 +0700

    Update check_data.py

--- check_data.py
+++ check_data.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle   
 
 print("===== ref_name.pkl =====")
 with open("ref_name.pkl", "rb") as f:
@@ -8,3 +8,4 @@ print("\n===== ref_embed.pkl =====")
 with open("ref_embed.pkl", "rb") as f:
     data = pickle.load(f)
     print({k: len(v) for k, v in data.items()})
+
